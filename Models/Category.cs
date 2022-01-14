bb778027109eae071d4724c38a9383a3363b0b76
Marcos <marquinhooliveira9968@outlook.com>
2022-01-13 23:57:39 -0300

    first commit

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="O número da Ordem de Exibição só pode ser entre 1 e 100!")]
+        public int DisplayOrder { get; set; }
+        
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
